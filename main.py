b00844cd886ed0726e74395380e38375509e1f97
Hugo Valdrez <hvaldrez98@gmail.com>
2025-02-04 18:00:15 +0000

    Data prep

--- main.py
+++ main.py
@@ -8,4 +8,5 @@ if __name__ == "__main__":
     df = asyncio.run(bgg.main(game_ids))
     
     df = data_prep.clean_text_column(df, "description")
+    df = data_prep.add_popularity_score(df)
     df.write_json("game_data.json")
